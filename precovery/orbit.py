aa35e1e74292a7ab7cc6d745440a493ac3772249
Spencer Nelson <swnelson@uw.edu>
2021-10-06 16:30:05 -0700

    Add code for generating ephemerides and propagating orbits
    Uses oorb. Kind of a scruffy API for now.

--- precovery/orbit.py
+++ precovery/orbit.py
@@ -1,11 +1,193 @@
+import enum
+
+import numpy as np
+import numpy.typing as npt
+import pyoorb
+
+pyoorb_initialized = False
+
+
+def _ensure_pyoorb_initialized(*args, **kwargs):
+    """Make sure that pyoorb is initialized."""
+    global pyoorb_initialized
+    if not pyoorb_initialized:
+        pyoorb.pyoorb.oorb_init(*args, **kwargs)
+
+
+class OrbitElementType(enum.Enum):
+    CARTESIAN = 1
+    COMETARY = 2
+    KEPLERIAN = 3
+
+
+class EpochTimescale(enum.Enum):
+    UTC = 1
+    UT1 = 2
+    TT = 3
+    TAI = 4
+
+
+class PropagationIntegrator(enum.Enum):
+    N_BODY = 1
+    TWO_BODY = 2
+
+
 class Orbit:
-    def __init__(self):
-        pass
+    def __init__(self, orbit_id: int, state_vector: npt.NDArray[np.float64]):
+        """
+        Create a new Orbit.
 
-    def propagate(self, timestamp: int) -> "PropagatedOrbit":
-        return PropagatedOrbit(self)
+        state_vector is a pretty opaque blob. It should be the structure that
+        pyoorb expects - a 12-element vector of doubles.
+        """
+        self.orbit_id = orbit_id
+        self._state_vector = state_vector
+        self._orbit_type = OrbitElementType(int(state_vector[0][7]))
+        self._epoch_timescale = EpochTimescale(int(state_vector[0][9]))
+        self._epoch = state_vector[0][8]
+
+    @classmethod
+    def cometary(
+        cls,
+        orbit_id: int,
+        perihelion_au: float,
+        eccentricity: float,
+        inclination_deg: float,
+        ascending_node_longitude_deg: float,
+        periapsis_argument_deg: float,
+        perihelion_epoch_mjd: float,
+        osculating_element_epoch_mjd: float,
+        epoch_timescale: EpochTimescale,
+        abs_magnitude: float,
+        photometric_slope_parameter: float,
+    ):
+        state_vector = np.array(
+            [
+                [
+                    orbit_id,
+                    perihelion_au,
+                    eccentricity,
+                    inclination_deg,
+                    ascending_node_longitude_deg,
+                    periapsis_argument_deg,
+                    perihelion_epoch_mjd,
+                    OrbitElementType.COMETARY.value,
+                    osculating_element_epoch_mjd,
+                    epoch_timescale.value,
+                    abs_magnitude,
+                    photometric_slope_parameter,
+                ]
+            ],
+            dtype=np.double,
+            order="F",
+        )
+
+        return cls(orbit_id, state_vector)
+
+    @classmethod
+    def keplerian(
+        cls,
+        orbit_id: int,
+        semimajor_axis_au: float,
+        eccentricity: float,
+        inclination_deg: float,
+        ascending_node_longitude_deg: float,
+        periapsis_argument_deg: float,
+        mean_anomaly_deg: float,
+        _,
+        osculating_element_epoch_mjd: float,
+        epoch_timescale: EpochTimescale,
+        abs_magnitude: float,
+        photometric_slope_parameter: float,
+    ):
+        state_vector = np.array(
+            [
+                [
+                    orbit_id,
+                    semimajor_axis_au,
+                    eccentricity,
+                    inclination_deg,
+                    ascending_node_longitude_deg,
+                    periapsis_argument_deg,
+                    mean_anomaly_deg,
+                    OrbitElementType.COMETARY.value,
+                    osculating_element_epoch_mjd,
+                    epoch_timescale.value,
+                    abs_magnitude,
+                    photometric_slope_parameter,
+                ]
+            ],
+            dtype=np.double,
+            order="F",
+        )
+
+        return cls(orbit_id, state_vector)
+
+    def propagate(
+        self, epoch: float, method: PropagationIntegrator = PropagationIntegrator.N_BODY
+    ) -> "Orbit":
+        _ensure_pyoorb_initialized()
+
+        epoch_array = np.array(
+            [epoch, self._epoch_timescale.value], dtype=np.double, order="F"
+        )
+
+        if method == PropagationIntegrator.N_BODY:
+            dynmodel = "N"
+        elif method == PropagationIntegrator.TWO_BODY:
+            dynmodel = "2"
+        else:
+            raise ValueError("unexpected propagation method %r" % method)
+
+        result, err = pyoorb.pyoorb.oorb_propagation(
+            in_orbits=self._state_vector,
+            in_epoch=epoch_array,
+            in_dynmodel=dynmodel,
+        )
+        assert err == 0
+        return Orbit(int(result[0][0]), result)
+
+    def compute_ephemeris(
+        self,
+        obscode: int,
+        epoch: float,
+        method: PropagationIntegrator = PropagationIntegrator.N_BODY,
+    ) -> "Ephemeris":
+        """
+        Compute ephemeris for the orbit, propagated to an epoch, and observed from
+        a location represented by obscode.
+
+        obscode should be a Minor Planet Center observatory code.
+        """
+        _ensure_pyoorb_initialized()
+        epoch_array = np.array(
+            [[epoch, self._epoch_timescale.value]], dtype=np.double, order="F"
+        )
+
+        if method == PropagationIntegrator.N_BODY:
+            dynmodel = "N"
+        elif method == PropagationIntegrator.TWO_BODY:
+            dynmodel = "2"
+        else:
+            raise ValueError("unexpected propagation method %r" % method)
+
+        eph, err = pyoorb.pyoorb.oorb_ephemeris_basic(
+            in_orbits=self._state_vector,
+            in_obscode=obscode,
+            in_date_ephems=epoch_array,
+            in_dynmodel=dynmodel,
+        )
+        assert err == 0
+        return Ephemeris(eph[0][0])
 
 
-class PropagatedOrbit:
-    def __init__(self, orbit: Orbit):
-        self.orbit = orbit
+class Ephemeris:
+    def __init__(self, raw_data: npt.NDArray[np.float64]):
+        self._raw_data = raw_data
+
+        self.mjd = raw_data[0]
+
+        self.ra = raw_data[1]
+        self.dec = raw_data[2]
+        self.ra_velocity = raw_data[3]  # deg per day
+        self.dec_velocity = raw_data[4]  # deg per day
---
+++ tests/test_orbit.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+from precovery import orbit
+
+
+def test_orbit_initialization_from_state_vector():
+    state_vector = np.array(
+        [
+            [
+                0,
+                1.46905,
+                0.33435,
+                np.deg2rad(14.3024),
+                np.deg2rad(224.513),
+                np.deg2rad(27.5419),
+                np.deg2rad(324.697),
+                3,
+                51544.5,
+                1,
+                12.5,
+                0.15,
+            ]
+        ],
+        dtype=np.double,
+        order="F",
+    )
+    o = orbit.Orbit(0, state_vector)
+    assert o._orbit_type == orbit.OrbitElementType.KEPLERIAN
+
+
+def test_orbit_propagation():
+    state_vector = np.array(
+        [
+            [
+                0,
+                1.46905,
+                0.33435,
+                np.deg2rad(14.3024),
+                np.deg2rad(224.513),
+                np.deg2rad(27.5419),
+                np.deg2rad(324.697),
+                3,
+                51544.5,
+                1,
+                12.5,
+                0.15,
+            ]
+        ],
+        dtype=np.double,
+        order="F",
+    )
+    o = orbit.Orbit(0, state_vector)
+
+    propagated = o.propagate(51232.23)
+    assert propagated._epoch == 51232.23
+
+    # Original should be unchanged
+    assert o._epoch == 51544.5
+
+
+def test_orbit_ephemeris_computation():
+    state_vector = np.array(
+        [
+            [
+                0,
+                1.46905,
+                0.33435,
+                np.deg2rad(14.3024),
+                np.deg2rad(224.513),
+                np.deg2rad(27.5419),
+                np.deg2rad(324.697),
+                3,
+                51544.5,
+                1,
+                12.5,
+                0.15,
+            ]
+        ],
+        dtype=np.double,
+        order="F",
+    )
+    o = orbit.Orbit(0, state_vector)
+
+    obscode = "534"
+    epoch = 51544.5
+
+    ephem = o.compute_ephemeris(obscode, epoch)
+    assert ephem.mjd == epoch
