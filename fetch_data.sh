c0cb3c991c047e6bad716b120219e0d55317df71
Spencer Nelson <swnelson@uw.edu>
2021-10-11 11:01:02 -0700

    Add script to fetch NSC test data

---
+++ fetch_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p data
+scp epyc.astro.washington.edu:/epyc/projects/thor/thor_data/nsc/hdf5/nsc_dr2_observations_2019-08-09_2019-09-09.h5 ./data/nsc_dr2_observations_2019-08-09_2019-09-09.h5
