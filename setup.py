648cc11ae9618cf14f8c470136e51e32e17a864e
Joachim Moeyens <moeyensj@gmail.com>
2022-09-02 13:02:03 -0700

    Use setuptools_scm for semantic versioning

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup
 
-setup()
+setup(
+    use_scm_version={
+        "write_to": "precovery/version.py",
+        "write_to_template": "__version__ = '{version}'",
+    }
+)
