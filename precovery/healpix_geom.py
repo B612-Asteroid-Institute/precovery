d4731c915af6520f1fe24756e67f642969260e1c
Spencer Nelson <spencer@b612foundation.org>
2023-01-24 16:56:23 -0800

    Mark healpix_geom.radec_to_healpixel as type-overloaded

--- precovery/healpix_geom.py
+++ precovery/healpix_geom.py
@@ -1,9 +1,21 @@
+from typing import overload
+
 import healpy as hp
 import numpy as np
 import numpy.typing as npt
 
 
+@overload
+def radec_to_healpixel(ra: float, dec: float, nside: int) -> int:
+    ...
+
+
+@overload
 def radec_to_healpixel(
     ra: npt.NDArray[np.float64], dec: npt.NDArray[np.float64], nside: int
 ) -> npt.NDArray[np.int64]:
+    ...
+
+
+def radec_to_healpixel(ra, dec, nside):
     return hp.ang2pix(nside, ra, dec, nest=True, lonlat=True).astype(np.int64)
