c89c2c648b18a9c95d16350bfb80272b5e0b8482
Spencer Nelson <spencer@b612foundation.org>
2023-01-31 11:22:09 -0800

    Remove unique constraint from fast_query index
    The uniqueness constraint can't be obeyed if exposure data
    is fed in across several passes, since rows are never updated
    in our current design.

--- precovery/frame_db.py
+++ precovery/frame_db.py
@@ -505,9 +505,7 @@ class FrameIndex:
             sq.Column("data_offset", sq.Integer),
             sq.Column("data_length", sq.Integer),
             # Create index on midpoint mjd, healpixel, obscode
-            sq.Index(
-                "fast_query", "exposure_mjd_mid", "healpixel", "obscode", unique=True
-            ),
+            sq.Index("fast_query", "exposure_mjd_mid", "healpixel", "obscode"),
         )
 
         self.datasets = sq.Table(
