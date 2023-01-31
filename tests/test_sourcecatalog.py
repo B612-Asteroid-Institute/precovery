acebf06df8920a7929b3577c3622e3087891527c
Spencer Nelson <spencer@b612foundation.org>
2023-01-31 10:49:26 -0800

    More tests of sourcecatalog bundling

--- tests/test_sourcecatalog.py
+++ tests/test_sourcecatalog.py
@@ -68,3 +68,122 @@ class TestBundleObservationsIntoFrames:
 
         result = list(frames)
         assert len(result) == 0
+
+    def test_single_exposure(self):
+        """Bundle a single exposure"""
+        observations = [make_sourceobs()]
+        frames = bundle_into_frames(observations)
+
+        result = list(frames)
+
+        assert len(result) == 1
+        frame = result[0]
+        assert len(frame.observations) == 1
+        assert frame.observations[0] == observations[0]
+
+    def test_single_frame(self):
+        """Bundle observations related to a single SourceFrame"""
+
+        # Use a constant exposure ID and healpixel, so everything gets one frame.
+        exp_id = "exposure"
+        healpixel = 1
+
+        observations = [
+            make_sourceobs(exposure_id=exp_id, healpixel=healpixel),
+            make_sourceobs(exposure_id=exp_id, healpixel=healpixel),
+            make_sourceobs(exposure_id=exp_id, healpixel=healpixel),
+            make_sourceobs(exposure_id=exp_id, healpixel=healpixel),
+        ]
+        frames = bundle_into_frames(observations)
+
+        result = list(frames)
+
+        assert len(result) == 1
+        frame = result[0]
+
+        assert len(frame.observations) == 4
+        assert frame.observations[0] == observations[0]
+
+    def test_multiple_frames_one_exposure(self):
+        # Use a constant exposure ID, but variable healpixels
+        exp_id = "exposure"
+
+        observations = [
+            make_sourceobs(exposure_id=exp_id, healpixel=1),
+            make_sourceobs(exposure_id=exp_id, healpixel=1),
+            make_sourceobs(exposure_id=exp_id, healpixel=1),
+            make_sourceobs(exposure_id=exp_id, healpixel=2),
+            make_sourceobs(exposure_id=exp_id, healpixel=2),
+            make_sourceobs(exposure_id=exp_id, healpixel=3),
+        ]
+        frames = bundle_into_frames(observations)
+
+        result = list(frames)
+
+        assert len(result) == 3
+        frame1 = result[0]
+
+        assert len(frame1.observations) == 3
+        assert frame1.healpixel == 1
+        assert frame1.observations[0] == observations[0]
+
+        frame2 = result[1]
+        assert len(frame2.observations) == 2
+        assert frame2.healpixel == 2
+        assert frame2.observations[0] == observations[3]
+
+        frame3 = result[2]
+        assert len(frame3.observations) == 1
+        assert frame3.healpixel == 3
+        assert frame3.observations[0] == observations[5]
+
+    def test_scrambled_order_within_exposure(self):
+        exp_id = "exposure"
+
+        observations = [
+            make_sourceobs(exposure_id=exp_id, healpixel=2),
+            make_sourceobs(exposure_id=exp_id, healpixel=1),
+            make_sourceobs(exposure_id=exp_id, healpixel=3),
+            make_sourceobs(exposure_id=exp_id, healpixel=1),
+            make_sourceobs(exposure_id=exp_id, healpixel=2),
+            make_sourceobs(exposure_id=exp_id, healpixel=1),
+        ]
+        frames = bundle_into_frames(observations)
+
+        result = list(frames)
+
+        assert len(result) == 3
+
+        assert any(f.healpixel == 1 for f in result)
+        assert any(f.healpixel == 2 for f in result)
+        assert any(f.healpixel == 3 for f in result)
+
+        for f in result:
+            if f.healpixel == 1:
+                assert len(f.observations) == 3
+            elif f.healpixel == 2:
+                assert len(f.observations) == 2
+            elif f.healpixel == 3:
+                assert len(f.observations) == 1
+            else:
+                assert False, f"unexpected healpixel: {f.healpixel}"
+
+    def test_multiple_exposures(self):
+        observations = [
+            # Frame 1:
+            make_sourceobs(exposure_id="e1", healpixel=1),
+            make_sourceobs(exposure_id="e1", healpixel=1),
+            # Frame 2:
+            make_sourceobs(exposure_id="e1", healpixel=2),
+            # Frame 3:
+            make_sourceobs(exposure_id="e2", healpixel=2),
+            # Frame 4:
+            make_sourceobs(exposure_id="e2", healpixel=1),
+            # Frame 5:
+            make_sourceobs(exposure_id="e3", healpixel=3),
+        ]
+        frames = bundle_into_frames(observations)
+
+        result = list(frames)
+
+        assert len(result) == 5
